3522a5c10c8d77225d690136abd1b5672b277795
Luyuan Nathan <lnathan@intrepid.io>
2018-09-25 16:34:23 -0700

    Create new Constants file

---
+++ Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  Pods-TestPod_Example
+//
+//  Created by Nathan, Lu on 9/25/18.
+//
+
+import UIKit
+
+public class Constants {
+
+    // MARK: Spacing
+    public enum Spacing: CGFloat {
+        case xs = 5
+        case sm = 10
+        case md = 15
+        case lg = 20
+        case xl = 35
+        case xxl = 50
+    }
+}
