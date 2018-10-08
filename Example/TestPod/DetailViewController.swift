5f02e55bd16dcc899955e35d52ea8f7e9a4f134b
Nathan, Lu <Lu.Nathan5@T-Mobile.com>
2018-10-07 22:44:14 -0400

    Create a split view controller, with associated table and detail VCs

---
+++ Example/TestPod/DetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.swift
+//  TestPod_Example
+//
+//  Created by Nathan, Lu on 10/8/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
