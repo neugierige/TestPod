751c9d645b0f0990466df2c970559dfa06ce3e80
Nathan, Lu <Lu.Nathan5@T-Mobile.com>
2018-10-28 21:08:11 -0400

    Adjust MasterVC to allow either push or show transitions

--- Example/TestPod/Components/PageTitlesTableViewController.swift
+++ Example/TestPod/Components/PageTitlesTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  PageTitlesController.swift
+//  PageTitlesTableViewController.swift
 //  TestPod_Example
 //
 //  Created by Nathan, Lu on 10/25/18.
@@ -9,13 +9,39 @@
 import UIKit
 import TestPod
 
-class PageTitlesController: UIViewController {
+class PageTitlesTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            return UITableViewCell()
+        }
+        switch indexPath.row {
+            case 0: cell.textLabel?.text = "Page Title"
+            case 1: cell.textLabel?.text = "Page Title with header"
+            default: break
+        }
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let emptyTable = UITableViewController()
+        switch indexPath.row {
+            case 1:
+                let tmoNavSubtitle = TMoNavigationController(rootViewController: emptyTable, title: "Line Setup", subtitle: "John's Line")
+                splitViewController?.showDetailViewController(tmoNavSubtitle, sender: nil)
+            default:
+                let tmoNav = TMoNavigationController(rootViewController: emptyTable, title: "All Plans", subtitle: nil)
+                splitViewController?.showDetailViewController(tmoNav, sender: nil)
+        }
+    }
 
 }
--- Example/TestPod/MasterViewController.swift
+++ Example/TestPod/MasterViewController.swift
@@ -7,16 +7,14 @@
 //
 
 import UIKit
+import TestPod
 
 class MasterViewController: UITableViewController {
-
-    let components: [Component] = [
-        Component(name: "CTAs", patternID: "17", storyboardID: nil),
-        Component(name: "Secondary CTA", patternID: "18", storyboardID: nil)]
 
     let componentCategories: [ComponentCategory] = [
         ComponentCategory(categoryName: "Buttons", subtitle: "View that have touch recognizer", storyboardID: "CTAsViewController", usageDescription: "lorem ipsem foo foo", bestPractices: "do this, not that", components: []),
         ComponentCategory(categoryName: "Icons", subtitle: "Icons and button images, credit card and social media logos", storyboardID: "IconsViewController", usageDescription: "lorem ipsem foo foo", bestPractices: "do this, not that", components: []),
+        ComponentCategory(categoryName: "Page Titles", subtitle: "Navigation Controllers with titles", storyboardID: "PageTitleTableViewController", usageDescription: "lorem ipsem foo foo", bestPractices: "do this, not that", components: []),
         ComponentCategory(categoryName: "Typography", subtitle: "Heading, paragraph, links", storyboardID: "TypographyViewController", usageDescription: "lorem ipsem foo foo", bestPractices: "do this, not that", components: [])
     ]
 
@@ -42,12 +40,22 @@ class MasterViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let storyboardName = componentCategories[indexPath.row].storyboardID
         let componentStoryboard = UIStoryboard(name: storyboardName, bundle: nil)
-        if let componentViewController = componentStoryboard.instantiateInitialViewController() {
-            let navVC = UINavigationController(rootViewController: componentViewController)
-            splitViewController?.showDetailViewController(navVC, sender: nil)
-        } else {
-            print("no VC for \(storyboardName)")
+        pushOrShowStoryboard(componentStoryboard)
+    }
+
+    func pushOrShowStoryboard(_ storyboard: UIStoryboard) {
+        if let viewController = storyboard.instantiateInitialViewController() {
+            pushOrShowViewController(viewController)
         }
     }
 
+    func pushOrShowViewController(_ viewController: UIViewController) {
+        if viewController is PageTitlesTableViewController {
+             navigationController?.pushViewController(viewController, animated: true)
+        } else {
+            let navVC = UINavigationController(rootViewController: viewController)
+            splitViewController?.showDetailViewController(navVC, sender: nil)
+        }
+
+    }
 }
